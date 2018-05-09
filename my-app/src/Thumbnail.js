0d11d935de38aef8972c0f1f837d2e6814056101
Ali Khan <aliimrankhan86@googlemail.com>
2018-05-09 17:42:57 +0100

    Some work

--- my-app/src/Thumbnail.js
+++ my-app/src/Thumbnail.js
@@ -6,31 +6,39 @@ class Thumbnail extends React.Component {
 
   constructor(props) {
     super(props)
+    this.t = 2;
+    console.log('Constructor has fired');
+
     this.state = {
       video: [
-        {name: 'New President', Description: '2018 Election Result', path: 'http://via.placeholder.com/200x150'},
-        {name: 'Energy Efficency', Description: 'Shell 2018 vision', path: 'http://via.placeholder.com/200x150'},
-        {name: 'Kwick Doctor', Description: 'Consultation Online', path: 'http://via.placeholder.com/200x150'}
+        {name: 'New President', Description: '2018 Election Result', id: 1, path: 'http://via.placeholder.com/200x150'},
+        {name: 'Energy Efficency', Description: 'Shell 2018 vision', id: 2, path: 'http://via.placeholder.com/200x150'},
+        {name: 'Kwick Doctor', Description: 'Consultation Online', id: 3, path: 'http://via.placeholder.com/200x150'}
       ]
     }
   }
+
+  componentDidMount() {
+    console.log('Component did Mount has fired');
+  }
+
   
 
+  test = () => {
+    let data = this.state.video;
+    data.push({name: 'Ali Khan', Description: 'React', path: 'http://via.placeholder.com/200x150'})
+    console.log(data);
+    // console.log(this.setState = {data});
+    this.setState({
+      video: data
+    })
+  }
+  
   
     render() {
-      let test = () => {
-        let data = this.state.video;
-        data.push({name: 'Ali Khan', Description: 'React', path: 'http://via.placeholder.com/200x150'})
-        console.log(data);
-        console.log(this.setState = {data});
-        this.setState = {data};
-      }
-
-
-      // console.log(this.state.video[0].name)
       return (
         <div>
-          <button onClick={test}>
+          <button onClick={this.test}>
             Append Data
           </button>
           <Thumbnailimage source={this.state.video}/>
--- my-app/src/Thumbnailimage.js
+++ my-app/src/Thumbnailimage.js
@@ -5,12 +5,22 @@ class Thumbnailimage extends React.Component {
   constructor(props) {
     super()
   }
+  loadVideo = (e, id) => {
+    console.log(e);
+
+    console.log(id);
+    // console.log(para1);
+  }
+
+
+
+
   render() {
     return (
       <div>
       { 
           this.props.source.map(item => {
-            return <div className="thumbnail">
+            return <div className="thumbnail" onClick={(event, id) => this.loadVideo(event, item.id) }>
               <img src ={item.path} /> 
                 <div className="thumbnailtext">
                   <p>{item.name}</p>
